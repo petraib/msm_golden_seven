4723a8a1407e06cc48137b17a88fbaaf9ef657cd
Petra Bakosova <petrab@chicagobooth.edu>
2017-07-21 05:04:15 +0000

    create and delete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,15 +6,42 @@ class MoviesController < ApplicationController
   end
 
   def d_index
-      
+     @all_directors = Director.all.order(:created_at => :desc)
        render("msm_templates/directors.html.erb") 
+  end
+  
+   def d_new
+      
+   render("msm_templates/directors_new.html.erb") 
+
+  end
+  
+  def d_process_new
+      
+     d=Director.new()
+     d.name = params[:name]
+     d.bio = params[:bio]
+     d.dob =  params[:dob]
+     d.image_url =  params[:image_url]
+
+     d.save
+     
+    @dir_id = d[:id]
+   render("msm_templates/directors_detail.html.erb") 
+
   end
   
   def d_detail
       
       @dir_id = params[:director_id]
-    
-      
       render("msm_templates/directors_detail.html.erb") 
   end
+  
+   def d_delete
+      
+      @dir_id = params[:director_id]
+      Director.destroy(@dir_id)
+      redirect_to("msm_templates/directors.html.erb") 
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,17 @@ Rails.application.routes.draw do
   
   get("/", { :controller => "movies", :action => "index" })
   
+  #create
+  get("/directors/new_form", { :controller => "movies", :action => "d_new" })
+  get("/directors/create", { :controller => "movies", :action => "d_process_new" })
+
   #read
   get("/directors", { :controller => "movies", :action => "d_index" })
   get("/directors/:director_id", { :controller => "movies", :action => "d_detail" })
 
+  #delete
+  get("/delete_director/:director_id", { :controller => "movies", :action => "d_delete" })
   
+  
+
 end
